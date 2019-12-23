dfca1fcf72b22c809b63a7cabdd801f91bb22327
Shahar Cohen <shaharcoh@gmail.com>
2019-12-23 14:22:24 +0000

    footer content

--- wp-content/themes/FIH/footer.php
+++ wp-content/themes/FIH/footer.php
@@ -27,7 +27,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						temp content
 
 					</div><!-- .site-info -->
 
